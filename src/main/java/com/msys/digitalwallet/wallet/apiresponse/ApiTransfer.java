13408e92d3ad71b05cfd7d1025fafeebd0348067
ajin <ajinalex05@gmail.com>
2023-10-06 17:43:28 +0530

    all the files are commited

---
+++ src/main/java/com/msys/digitalwallet/wallet/apiresponse/ApiTransfer.java
@@ -0,0 +1,40 @@
+package com.msys.digitalwallet.wallet.apiresponse;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiTransfer {
+
+
+  private String transactionID;
+
+
+  private String userId;
+
+
+  private Double amount;
+
+
+  private String currency;
+
+
+  private LocalDateTime transacDateTime;
+
+
+  private String transactionStatus;
+
+  private String accountIDFrom;
+
+
+  private String accountIDFromType;
+
+  private String fromUserEmail;
+
+  private String accountIDTo;
+
+  private String accountIdToType;
+
+  private String toUserEmail;
+}
---
+++ src/main/java/com/msys/digitalwallet/wallet/mapper/TransferMapper.java
@@ -0,0 +1,35 @@
+package com.msys.digitalwallet.wallet.mapper;
+
+import com.msys.digitalwallet.wallet.apiresponse.ApiTransfer;
+import com.msys.digitalwallet.wallet.enums.Currency;
+import com.msys.digitalwallet.wallet.model.Transfer;
+import org.mapstruct.Mapper;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Mapper(componentModel = "spring")
+public interface TransferMapper {
+
+
+    ApiTransfer fromTransfer(Transfer transfer);
+
+    default Transfer toTransfer(String userId, String transactionId, String fromAccountId, String fromUserEmail,String toAccountId, String toUserEmail,BigDecimal amount, Currency currency, String reason, String status){
+        return Transfer.builder()
+                .accountIDFrom(fromAccountId)
+                .fromUserEmail(fromUserEmail)
+                .accountIDTo(toAccountId)
+                .toUserEmail(toUserEmail)
+                .accountIDFromType("Wallet")
+                .accountIdToType("Wallet")
+                .amount(amount.doubleValue())
+                .currency(currency.name())
+                .userId(userId)
+                .transactionID(transactionId)
+                .reason(reason)
+                .transacDateTime(LocalDateTime.now())
+                .transactionStatus(status)
+                .build();
+    }
+
+}
